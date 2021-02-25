50255db995d61a63105103ff53496fc308f64127
Yehor Kolodii <yegorkolodii@gmail.com>
2021-02-26 00:59:32 +0200

    init commit

---
+++ task_2.py
@@ -0,0 +1,15 @@
+import argparse
+import operator
+
+parser = argparse.ArgumentParser(description='Standard math operations')
+
+parser.add_argument('operator')
+parser.add_argument('first_value')
+parser.add_argument('second_value')
+
+args = parser.parse_args()
+
+try:
+    print(eval('operator.' + args.operator + '(' + args.first_value + ',' + args.second_value + ')'))
+except:
+    raise NotImplementedError
