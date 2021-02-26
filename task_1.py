aa9e15b7ff43821984915c1bebdc1ad0cc910c06
Yehor Kolodii <yegorkolodii@gmail.com>
2021-02-27 01:02:31 +0200

    remove eval

--- task_1.py
+++ task_1.py
@@ -1,16 +1,28 @@
 import argparse
 
 parser = argparse.ArgumentParser(description='Simple arithmetic operations')
+parser.add_argument('operand1', type=float, help='first operand')
+parser.add_argument('operator', type=str, help='type of operator: +, -, /, *')
+parser.add_argument('operand2', type=float, help='second operand')
 
-operations = ('+', '-', '/', '*')
 
-parser.add_argument('first_value')
-parser.add_argument('operator', help='type of operator: +, -, /, *')
-parser.add_argument('second_value')
+def calculator(args):
+    if args.operator == '+':
+        return args.operand1 + args.operand2
+    elif args.operator == '-':
+        return args.operand1 - args.operand2
+    elif args.operator == '/':
+        return args.operand1 / args.operand2
+    elif args.operator == '*':
+        return args.operand1 * args.operand2
+    else:
+        raise NotImplementedError(f"passed operation {args.operator} not implemented!")
 
-args = parser.parse_args()
 
-if args.operator in operations and args.first_value.isnumeric() and args.second_value.isnumeric():
-    print(eval(args.first_value + args.operator + args.second_value))
-else:
-    raise NotImplementedError
+def main():
+    args = parser.parse_args()
+    print(calculator(args))
+
+
+if __name__ == '__main__':
+    main()
