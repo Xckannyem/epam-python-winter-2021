18d988d492b078d793fa58ebbc6c20e674fc0b65
Yehor Kolodii <yegorkolodii@gmail.com>
2021-04-15 05:44:51 +0300

    make solution more clear and correct

--- task_1/task_1_ex_1.py
+++ task_1/task_1_ex_1.py
@@ -7,16 +7,19 @@ parser.add_argument('operand2', type=float, help='second operand')
 
 
 def calculator(args):
-    if args.operator == '+':
-        return args.operand1 + args.operand2
-    elif args.operator == '-':
-        return args.operand1 - args.operand2
-    elif args.operator == '/':
-        return args.operand1 / args.operand2
-    elif args.operator == '*':
-        return args.operand1 * args.operand2
-    else:
-        raise NotImplementedError(f"passed operation {args.operator} not implemented!")
+    operand1 = args.operand1
+    operator = args.operator
+    operand2 = args.operand2
+
+    operations = {
+        '+': lambda x, y: x + y,
+        '-': lambda x, y: x - y,
+        '*': lambda x, y: x * y,
+        '/': lambda x, y: x / y,
+    }
+    if operator not in operations:
+        raise NotImplementedError(f'passed operation {operator} is not implemented!')
+    return operations[operator](operand1, operand2)
 
 
 def main():
--- task_1/task_1_ex_2.py
+++ task_1/task_1_ex_2.py
@@ -3,25 +3,19 @@ import math
 import operator
 
 parser = argparse.ArgumentParser(description='Standard math operations')
-parser.add_argument('expression', nargs='*')
+parser.add_argument('operand', type=str)
+parser.add_argument('numbers', type=float, nargs='*')
 
 
 def calculator(args):
-    operator_expr = args.expression[0]
-    full_expr = args.expression[0] + '(' + ','.join(args.expression[1:]) + ')'
+    operand = args.operand
+    numbers = args.numbers
 
-    if operator_expr in dir(math):
-        if len(args.expression) == 1:
-            return eval('math.' + operator_expr)
-        else:
-            return eval('math.' + full_expr)
-    elif operator_expr in dir(operator):
-        if len(args.expression) == 1:
-            return eval('operator.' + operator_expr)
-        else:
-            return eval('operator.' + full_expr)
-    else:
-        raise NotImplementedError
+    if operand in dir(math):
+        return getattr(math, operand)(*numbers)
+    elif operand in dir(operator):
+        return getattr(operator, operand)(*numbers)
+    raise NotImplementedError
 
 
 def main():
